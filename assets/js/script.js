73981050d86b5a0cfabe3b806494dd0cffef6269
Deepika Bekal <deepika.bekal2@gmail.com>
2021-03-27 00:00:12 +0400

    color coded time blocks and added styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-var currentDate = moment().format("dddd, Do MMMM YYYY");
-console.log(currentDate);
+var timeOfTheDay = moment().hour(); //get current time in hour
+var currentDate = moment().format("dddd, Do MMMM YYYY"); //get the current date
+
+
+//console.log(currentDate);
 
 //display current date
 $("#currentDay").text(currentDate);
+
+//color code the time blocks to indicate past, present and future
+for (var i=9;i<17;i++){
+    var index = i-9;
+    if (i===timeOfTheDay){
+        $(".hour").eq(index).next().addClass("present");
+    } else if (i<timeOfTheDay){
+        $(".hour").eq(index).next().addClass("past");
+    } else {
+        $(".hour").eq(index).next().addClass("future");
+    }
+    
+}
+
