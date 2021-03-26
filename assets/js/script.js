d82263cc22a7ee921a43b1ea5a44fc8a9e21357f
Deepika Bekal <deepika.bekal2@gmail.com>
2021-03-27 00:53:10 +0400

    Add text to the time block on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
 var timeOfTheDay = moment().hour(); //get current time in hour
 var currentDate = moment().format("dddd, Do MMMM YYYY"); //get the current date
 
-
-//console.log(currentDate);
-
 //display current date
 $("#currentDay").text(currentDate);
 
 //color code the time blocks to indicate past, present and future
-for (var i=9;i<17;i++){
+for (var i=9;i<18;i++){
+    //debugger;
     var index = i-9;
     if (i===timeOfTheDay){
         $(".hour").eq(index).next().addClass("present");
@@ -19,4 +17,21 @@ for (var i=9;i<17;i++){
     }
     
 }
+
+//click the block to enter an event.
+$(".add-task").click(function(){
+    var getClass = $(this).attr("class");
+    console.log(getClass);
+    var taskText = $(this).text().trim();
+    var textInput = $("<textarea>").addClass(getClass).val(taskText);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+
+});
+
+$(".add-task").on("blur", function(){
+
+
+})
+
 
