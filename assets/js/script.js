3853e45b7b663575b0757f33a18f28082c928f3f
Deepika Bekal <deepika.bekal2@gmail.com>
2021-03-28 15:36:17 +0400

    local storage task retrieving fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var timeOfTheDay = moment().hour(); //get current time in hour
 var currentDate = moment().format("dddd, Do MMMM YYYY"); //get the current date
 var getTaskClass = ""; //get the class of the task saved
 var getTaskId = ""; //get the id of the task saved
+var taskText = "";
 var saveTask = [];
 
 //display current date
@@ -23,17 +24,25 @@ for (var i=9;i<18;i++){
 
 //click the block to enter an event.
 $(".row").on("click",".add-task",function(){
-
+    
     //save the edited time blocks id and class
     getTaskClass = $(this).attr("class");
     getTaskId = $(this).attr("id");
-    //console.log(getTaskClass);
+    console.log(getTaskClass);
+    console.log(getTaskId);
 
-    //get the text entered by the user
-    var taskText = $(this).text().trim();
+   if ($(this).text()!== ""){
+        //get the text entered by the user
+        taskText = $(this).text().trim();
+        console.log(taskText);
+   } else {
+       taskText = "";
+   }
 
+   console.log(taskText);
     //change td element to textarea
     var textInput = $("<textarea>").addClass(getTaskClass).val(taskText);
+    textInput.attr("id",getTaskId);
     $(this).replaceWith(textInput);
     //console.log(savedTaskId);
 
@@ -44,27 +53,35 @@ $(".row").on("click",".add-task",function(){
 $(".saveBtn").on("click", function(){
 
     //change textarea element back to td element
-    var taskText = $(this).prev().val().trim();
+    taskText = $(this).prev().val().trim();
+    console.log(taskText);
+
+    if (taskText===""){
+        alert("Please enter valid text.");
+        return;
+        
+    }
 
     var taskTd = $("<td>").addClass(getTaskClass);
     $(taskTd).attr("id", getTaskId);
     $(taskTd).text(taskText);
 
-    $(this).prev().replaceWith(taskTd);     
+    $(this).prev().replaceWith(taskTd);  
 
     //get the saved task from the local storage
-    getTask = JSON.parse(localStorage.getItem("workScheduler")) || [];
-    console.log("savetask",saveTask);    
-
-    //object to save the task id and task content
-    taskObject = {date:currentDate,id:getTaskId,textValue:taskText};
-    console.log("taskObject",taskObject);
-
-    //push th eobject in the array
-    saveTask.push(taskObject);
-
-    //save the array in the local storage
-    localStorage.setItem("workScheduler", JSON.stringify(saveTask));
+     saveTask = JSON.parse(localStorage.getItem("workScheduler")) || [];
+     console.log("savetask",saveTask);    
+ 
+     //object to save the task id and task content
+     taskObject = {date:currentDate,id:getTaskId,textValue:taskText};
+     console.log("taskObject",taskObject);
+    
+     //push th eobject in the array
+     saveTask.push(taskObject);
+     console.log(saveTask);
+ 
+     //save the array in the local storage
+     localStorage.setItem("workScheduler", JSON.stringify(saveTask));
 
 });
 
